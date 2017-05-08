bfe15fd8b5b8cdf0ac44fe04fd5ffc29068922af
timour.meeusen <timour.meeusen@B209-11>
2017-05-08 11:18:22 +0200

    Cirkel

---
+++ H24/Cirkel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Shapes;
+using System.Windows.Controls;
+
+namespace H24
+{
+    class Cirkel : Vorm
+    {
+        private Ellipse ell;
+
+        public Cirkel(int beginX, int beginY)
+        {
+            x = beginX;
+            y = beginY;
+
+            MaakCirkel();
+        }
+
+        private void MaakCirkel()
+        {
+            ell = new Ellipse();
+            ell.Width = grootte;
+            ell.Height = ell.Width;
+            ell.Stroke = brush;
+            ell.Margin = new Thickness(x, y, 0, 0);
+        }
+
+        public override void TonenOp(Canvas tekencanvas)
+        {
+            tekencanvas.Children.Add(ell);
+        }
+    }
+}
--- H24/Vierkant.cs
+++ H24/Vierkant.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Media;
 using System.Windows.Controls;
 using System.Windows.Shapes;
@@ -23,12 +24,16 @@ namespace H24
 
         private void MaakVierkant()
         {
-
+            rect = new Rectangle();
+            rect.Width = grootte;
+            rect.Height = grootte;
+            rect.Stroke = brush;
+            rect.Margin = new Thickness(x, y, 0, 0);
         }
 
         public override void TonenOp(Canvas tekencanvas)
         {
-
+            tekencanvas.Children.Add(rect);
         }
     }
 }
